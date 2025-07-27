6cbde5950b44d0696ba9018f857226fb58a64d1a
Youngkwang Yang <me@youngkwang.dev>
2025-07-27 18:00:26 +0900

    temp

--- auditlog/models.py
+++ auditlog/models.py
@@ -183,7 +183,7 @@ class LogEntryManager(models.Manager):
             )
         else:
             # Convert all PKs to strings for object_pk comparison
-            primary_keys = [smart_str(pk) for pk in primary_keys]
+            # primary_keys = [smart_str(pk) for pk in primary_keys]
             return (
                 self.filter(content_type=content_type)
                 .filter(Q(object_pk__in=primary_keys))
